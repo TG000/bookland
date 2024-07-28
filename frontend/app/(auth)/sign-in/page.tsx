cc0caef033a57427e8a47878dc04ab58bbcf0c6b
TLG <tranlangiac@gmail.com>
2024-07-28 14:43:21 +0700

    Quick setup for Shadcn installation

--- frontend/app/(auth)/sign-in/page.tsx
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SignInPage = () => {
-	return <>SignInPage</>;
+	return (
+		<>
+			<div className="flex-center flex-col">
+				<h1 className="h1-bold text-normal mb-5">SIGN IN</h1>
+			</div>
+		</>
+	);
 };
 
 export default SignInPage;
