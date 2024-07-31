0876a105a1255db44867060a3b3d356980899765
TLG <tranlangiac@gmail.com>
2024-08-01 05:33:57 +0700

    fix: Remove OAuthProvider type

--- constants/provider_constants.ts
+++
@@ -1,9 +0,0 @@
-import { OAuthProvider } from "@/types/auth";
-
-const GoogleProvider: OAuthProvider = {
-	id: "google",
-	name: "Google",
-	redirectURI: process.env.LUCIA_GOOGLE_REDIRECT_URI,
-};
-
-export { GoogleProvider };
--- lib/utils.ts
+++ lib/utils.ts
@@ -3,16 +3,20 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function getOAuthIcon(icon: string) {
-	switch (icon) {
-		case "google":
-			return googleIcon;
-		case "facebook":
-			return facebookIcon;
-		default:
-			return null;
-	}
+    switch (icon) {
+        case "google":
+            return googleIcon;
+        case "facebook":
+            return facebookIcon;
+        default:
+            return null;
+    }
+}
+
+export function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
