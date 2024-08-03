9948b206573b2f74376323d23087080f25571704
TLG <tranlangiac@gmail.com>
2024-08-04 06:15:20 +0700

    fix: Quick fix for SignOutForm

--- components/forms/SignOutForm.tsx
+++ components/forms/SignOutForm.tsx
@@ -2,15 +2,10 @@
 
 import { signOut } from "@/lib/actions/user.action";
 import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
 
 const SignOutForm = () => {
-    const router = useRouter();
-
     const logout = async () => {
         await signOut();
-
-        router.push("/sign-in");
     };
 
     return <Button onClick={logout}>Sign Out</Button>;
